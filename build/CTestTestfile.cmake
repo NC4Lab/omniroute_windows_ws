15ef6b29c9047d741645182be4d4dd217dd53798
afsoongh <afsoon.gharib@gmail.com>
2023-09-17 05:43:42 -0700

    Initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/NC4/nc4_code_base/omniroute_operation_windows_ws/src
+# Build directory: C:/Users/NC4/nc4_code_base/omniroute_operation_windows_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = 'C:/Users/NC4/nc4_code_base/omniroute_operation_windows_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = 'C:/opt/ros/noetic/x64;c:/opt/ros/noetic/x64'.split(';') if 'C:/opt/ros/noetic/x64;c:/opt/ros/noetic/x64' != '' else []
