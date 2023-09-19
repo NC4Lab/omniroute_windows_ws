ca5d77cdbb4379c663842d7488c79940c74a9f3c
AdamWardLester <adamwardlester@gmail.com>
2023-09-18 19:50:17 -0700

    Added a bat file for setup

---
+++ run_ws.bat
@@ -0,0 +1,20 @@
+@echo off  :: Turn off command echoing to clean up the output
+
+:: Open a new terminal and run roscore
+::start cmd /k "call C:\opt\ros\noetic\x64\setup.bat && roscore"
+
+:: Pause to give roscore time to start up
+::timeout /t 5
+
+:: Navigate to your ROS workspace
+cd C:\nc4_code_base\omniroute_operation_windows_ws
+
+:: Source the workspace to make sure the new build is recognized
+devel\setup.bat
+
+:: Build the ROS workspace using catkin_make
+call catkin_make
+
+:: Run the specific ROS node
+call rosrun goodbuy_world_pkg goodbuy_world_pkg_node
+
