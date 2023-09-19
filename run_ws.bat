f727332d297f806598033f399ce51c8faea5aa5b
AdamWardLester <adamwardlester@gmail.com>
2023-09-19 00:10:13 -0700

    still working on adding packages

--- run_ws.bat
+++ run_ws.bat
@@ -1,23 +1,23 @@
 @echo off  :: Turn off command echoing to clean up the output
 
-:: Open a new terminal and run roscore
-::start cmd /k "call C:\opt\ros\noetic\x64\setup.bat && roscore"
+@REM :: Open a new terminal and run roscore
+@REM start cmd /k "call C:\opt\ros\noetic\x64\setup.bat && roscore"
 
-:: Pause to give roscore time to start up
-::timeout /t 5
+@REM :: Pause to give roscore time to start up
+@REM timeout /t 5
 
-:: Source the ROS setup file
-::call C:\opt\ros\noetic\x64\setup.bat
+@REM :: Source the ROS setup file
+@REM call C:\opt\ros\noetic\x64\setup.bat
 
 :: Navigate to your ROS workspace
 cd C:\nc4_code_base\omniroute_windows_ws
 
 :: Source the workspace to make sure the new build is recognized
-devel\setup.bat
+call devel\setup.bat
 
-:: Build the ROS workspace using catkin_make
-call catkin_make
+:: Build the ROS workspace and run the specific ROS node
+catkin_make && rosrun goodbuy_world_pkg goodbuy_world_node
 
-:: Run the specific ROS node
-call rosrun goodbuy_world_pkg goodbuy_world_pkg_node
+@REM :: Build the ROS workspace and run the specific ROS node
+@REM catkin_make && rosrun projection_calibration projection_calibration_node
 
