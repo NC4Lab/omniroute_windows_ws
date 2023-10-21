06b8e1a3759239ee250212aa9ec6fbbbe0d8625e
AdamWardLester <adamwardlester@gmail.com>
2023-10-20 19:17:08 -0700

    Added ROS network test code to goodbuy_world

--- run_proj_cal.bat
+++ run_proj_cal.bat
@@ -5,6 +5,8 @@ call run_cmake.bat
 
 :: Run the specific ROS node
 echo Running Package EXE
+rosrun projection_operation projection_calibration_node
+
+:: Other nodes 
 ::rosrun goodbuy_world_pkg goodbuy_world_node
 ::rosrun proj_pkg_test proj_pkg_test_node
-rosrun projection_operation projection_calibration_node
