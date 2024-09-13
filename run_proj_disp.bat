13143b7bc10b188c53a7d716bcfc1aba5687ece7
AdamWardLester <adamwardlester@gmail.com>
2024-09-13 15:20:11 -0700

    Minor.

--- run_proj_disp.bat
+++ run_proj_disp.bat
@@ -7,3 +7,13 @@ call run_cmake.bat
 echo Running projection_display EXE
 rosrun projection_operation projection_display_node
 
+@REM :: Run the projection_display ROS node in the background
+@REM echo Running projection_display EXE
+@REM start /B rosrun projection_operation projection_display_node
+
+@REM :: Run the projection_sender ROS node in the background
+@REM echo Running Python projection_sender node
+@REM start /B rosrun projection_operation projection_sender.py
+
+@REM :: Prevent terminal from closing immediately
+@REM cmd /K
