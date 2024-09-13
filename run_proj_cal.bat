d198b164d83c1d1b3678d904f8b1614f84f2200d
AdamWardLester <adamwardlester@gmail.com>
2024-09-13 15:18:06 -0700

    Deleted projection sender test code.

--- run_proj_cal.bat
+++ run_proj_cal.bat
@@ -3,10 +3,6 @@
 :: Run catkin_make BAT file to build the ROS workspace
 call run_cmake.bat
 
-:: Run the specific ROS node
+:: Run the projection_calibration ROS node
 echo Running Package EXE
 rosrun projection_operation projection_calibration_node
-
-:: Other nodes 
-::rosrun goodbuy_world_pkg goodbuy_world_node
-::rosrun proj_pkg_test proj_pkg_test_node
--- run_proj_disp.bat
+++ run_proj_disp.bat
@@ -3,14 +3,7 @@
 :: Run catkin_make BAT file to build the ROS workspace
 call run_cmake.bat
 
-:: Run the projection_display ROS node in the background
+:: Run the projection_display ROS node 
 echo Running projection_display EXE
-start /B rosrun projection_operation projection_display_node
-
-:: Run the projection_sender ROS node in the background
-echo Running Python projection_sender node
-start /B rosrun projection_operation projection_sender.py
-
-:: Prevent terminal from closing immediately
-cmd /K
+rosrun projection_operation projection_display_node
 
