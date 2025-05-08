3f6651cec06b454822a397df644b002bc0de8be3
AdamWardLester <adamwardlester@gmail.com>
2025-05-08 16:09:05 -0700

    some edits in the sound generator code

--- src/projection_operation/src/sound_generator/sound_generator.py
+++ src/projection_operation/src/sound_generator/sound_generator.py
@@ -6,7 +6,7 @@ import os
 import numpy as np
 
 class SoundClip:
-    def __init__(self, file_name, duration, amplitude=1.0):
+    def __init__(self, file_name, duration, amplitude=1.0, preload = False):
         self.file_path = os.path.join(os.path.dirname(__file__), 'sound_clips', file_name)
         self.duration = duration
         self.amplitude = amplitude
@@ -14,7 +14,11 @@ class SoundClip:
         self.clip = []
         self.loaded = False
 
+        if preload:
+            self.load_clip()
+
     def load_clip(self):
+        rospy.loginfo(f'Loading sound clip: {self.file_path}')
         self.samplerate, self.clip = wavfile.read(self.file_path)
         rospy.loginfo('Loaded sound clip: ' + self.file_path)
         rospy.loginfo('Sample rate: ' + str(self.samplerate))
@@ -30,13 +34,19 @@ class SoundClip:
 
         # Normalize the clip
         self.clip = self.amplitude*self.clip/np.max(np.abs(self.clip))
+        self.loaded = True
 
     def play(self, device):
         rospy.loginfo('Playing sound clip: ' + self.file_path + ' on device: ' + device)
         if not self.loaded:
-            self.load_clip()    # Load the clip if it hasn't been loaded yet
-            self.loaded = True
-        sd.play(self.clip, self.samplerate, device=device)
+            rospy.logwarn(f'Sound {self.file_path} not preloaded, loading now...')
+            self.load_clip()
+
+        rospy.loginfo(f'Playing {self.file_path}')
+        if device:
+            sd.play(self.clip, self.samplerate, device=device)
+        else:
+            sd.play(self.clip, self.samplerate)
 
 class SoundGenerator:
     def __init__(self):
@@ -50,21 +60,26 @@ class SoundGenerator:
             rospy.loginfo(device['name'])
 
         # Clips dictionary
-        self.clips = {'White_Noise': SoundClip('whitenoise.wav', 5, 0.5),
-                        '5KHz': SoundClip('5khz.wav', 5.0),
-                        'Error': SoundClip('wrong-answer-buzz.wav', 8.0),
-                        '1KHz': SoundClip('1kHz.wav', 0.5, 0.5),
-                        '8KHz': SoundClip('8kHz.wav', 0.5, 0.5),
-                        'White_Noise_Training': SoundClip('white-noise-2min.wav', 120.0),
-                        '5KHz_Training': SoundClip('5khz-2min.wav', 120.0),
-                        'Starting_Sound': SoundClip('1kHz.wav', 0.5, 0.5)}
+        self.clips = {'White_Noise': SoundClip('whitenoise.wav', 5, 0.5, preload=True),
+                        '5KHz': SoundClip('5khz.wav', 5.0, preload=True),
+                        'Error': SoundClip('wrong-answer-buzz.wav', 8.0, preload=True),
+                        '1KHz': SoundClip('1kHz.wav', 0.5, 0.5, preload=True),
+                        '8KHz': SoundClip('8kHz.wav', 0.5, 0.5, preload=True),
+                        'White_Noise_Training': SoundClip('white-noise-2min.wav', 120.0, preload=True),
+                        '5KHz_Training': SoundClip('5khz-2min.wav', 120.0, preload=True),
+                        'Starting_Sound': SoundClip('1kHz.wav', 0.5, 0.5, preload=True),
+                        '1KHz_120s': SoundClip('tone_1000Hz_120.0s.wav', 120.0, 0.5, preload=True),
+                        '8KHz_120s': SoundClip('tone_8000Hz_120.0s.wav', 120.0, 0.5, preload=True)
+                        }
         
         # Create a subscriber for the sound topic
         self.sound_sub = rospy.Subscriber('/sound_cmd', String, self.sound_callback)
         
-        r = rospy.Rate(10)
-        while not rospy.is_shutdown():
-            r.sleep()
+        #r = rospy.Rate(10)
+        #while not rospy.is_shutdown():
+        #    r.sleep()
+
+        rospy.loginfo('SoundGenerator initialized successfully.')
 
     def sound_callback(self, msg):
         rospy.loginfo('Sound received: ' + msg.data)
